a8a6a1f849ad1862f7fe2c1c95e89c8fad2f3582
aksh-bigd-dev <akshdagar02@gmail.com>
2025-10-02 17:48:50 +0530

    installing modules

---
+++ frontend/iChats/src/components/Start.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Start = () => {
+
+const logo_entry = () => {
+  return 
+}
+
+
+useEffect(() => {
+  setTimeout(logo_entry, 1000);
+
+  return () => {
+    <Login/>
+  }
+}, [])
+
+
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default Login
